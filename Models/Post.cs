60df393443d1072d001ffd1d3c8bf7e4c38b4b34
acieffe <acieffe@gmail.com>
2021-11-02 13:13:05 -0400

    Added Post Model/Class

---
+++ Models/Post.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBlogProject.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Blog Name")]
+        public string PostId { get; set; }
+
+        public string Author { get; set; }
+
+        [Required]
+        [StringLength(75, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 2)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(200, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 2)]
+        public string Abstract { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Created On")]
+        public DateTime Created { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Updated On")]
+        public DateTime? Updated { get; set; }
+
+        public bool IsReady { get; set; }
+
+        public string Slug { get; set; }
+
+        [Display(Name = "Blog Image")]
+        public byte[] ImageData { get; set; }
+
+        [Display(Name = "Image Type")]
+        public string ContentType { get; set; }
+
+        [NotMapped]
+        public IFormFile Image { get; set; }
+    }
+}

8c3871fbd4ef0e9f8ccee81489fb55a7eb2b9cfc
acieffe <acieffe@gmail.com>
2021-11-02 20:33:55 -0400

    Change Author to BlogUser

--- Models/Post.cs
+++ Models/Post.cs
@@ -17,7 +17,7 @@ namespace MyBlogProject.Models
         [Display(Name = "Blog Name")]
         public string BlogtId { get; set; }
 
-        public string AuthorId { get; set; }
+        public string BlogUserId { get; set; }
 
         [Required]
         [StringLength(75, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 2)]
@@ -53,7 +53,7 @@ namespace MyBlogProject.Models
 
         //Navigation Properties
         public virtual Blog Blog { get; set; }
-        public virtual BlogUser Author { get; set; }
+        public virtual BlogUser BlogUser { get; set; }
         public virtual ICollection<Tag> Tags { get; set; } = new HashSet<Tag>();
         public virtual ICollection<Comment> Comments { get; set; } = new HashSet<Comment>();
 
--- Models/Tag.cs
+++ Models/Tag.cs
@@ -11,7 +11,7 @@ namespace MyBlogProject.Models
     {
         public int Id { get; set; }
         public int PostId { get; set; }
-        public string AuthorId { get; set; }
+        public string BlogUserId { get; set; }
 
         [Required]
         [StringLength(25, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 2)]
@@ -20,7 +20,7 @@ namespace MyBlogProject.Models
 
         //Navigation Properties
         public virtual Post Post { get; set; }
-        public virtual BlogUser Author { get; set; }
+        public virtual BlogUser BlogUser { get; set; }
 
     }
 }

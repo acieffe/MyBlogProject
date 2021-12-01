94e6185e9445d3d57715beea5d172b86be91d8b5
acieffe <acieffe@gmail.com>
2021-11-30 21:44:38 -0500

    Added scaffolding for Blog

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyBlogProject.Data;
+using MyBlogProject.Models;
+
+[assembly: HostingStartup(typeof(MyBlogProject.Areas.Identity.IdentityHostingStartup))]
+namespace MyBlogProject.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
